399fddf4387add47f09ca3ea4fc3fcf8cb5f0836
Stefano Pigozzi <me@steffo.eu>
2023-01-16 17:08:30 +0100

    Configure sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
     images: {
         domains: [],
     },
+    sentry: {
+        hideSourceMaps: false,
+    }
 }
+
+const sentryWebpackPluginOptions = {
+    url: "https://sentry.io/",
+    org: "stefano-pigozzi",
+    project: "steffoweb",
+    silent: true,
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
