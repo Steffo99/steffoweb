b9e0760c50b599ff3235eddf6a105283cc18162e
Stefano Pigozzi <me@steffo.eu>
2023-07-07 02:36:07 +0200

    Get rid of Sentry :(

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const { withSentryConfig } = require('@sentry/nextjs');
-
-const moduleExports = {
+module.exports = {
     images: {
         domains: [],
-    },
-    sentry: {
-        hideSourceMaps: false,
     }
 }
-
-const sentryWebpackPluginOptions = {
-    url: "https://sentry.io/",
-    org: "stefano-pigozzi",
-    project: "steffoweb",
-    silent: true,
-};
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
