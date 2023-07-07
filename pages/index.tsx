0825670441a16c0c95cb927374355636340d15e7
Stefano Pigozzi <me@steffo.eu>
2023-07-07 03:24:10 +0200

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import type {NextPage} from "next"
 import {default as Link} from "next/link"
 import {faGithub, faDiscord, faSteam, faItchIo, faLinkedin, faStackOverflow} from "@fortawesome/free-brands-svg-icons"
-import {faBorderAll, faEnvelope, faGlobe, faComment, faPlus, faCashRegister, faShieldAlt, faDiagramProject, faPaintRoller, faBookAtlas, faGamepad, faPaintbrush, faArrowLeft, faCog, faFloppyDisk, faScissors, faShield, faCircleNodes} from "@fortawesome/free-solid-svg-icons"
+import {faBorderAll, faEnvelope, faGlobe, faPlus, faCashRegister, faShieldAlt, faDiagramProject, faPaintRoller, faBookAtlas, faGamepad, faPaintbrush, faArrowLeft, faCog, faFloppyDisk, faScissors, faShield, faCircleNodes} from "@fortawesome/free-solid-svg-icons"
 import {FlipPanel} from "../components/FlipPanel"
 import {LinkPanel} from "../components/LinkPanel"
 import {ProjectsList} from "../components/ProjectsList"
